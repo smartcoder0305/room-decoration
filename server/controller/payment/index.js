5debc38051bfe01a8407f657c9f51071aabec97c
smartcoder0305 <102741052+smartcoder0305@users.noreply.github.com>
2023-01-24 04:57:25 -0800

    Fix the amount to 0.5

--- server/controller/payment/index.js
+++ server/controller/payment/index.js
@@ -77,9 +77,9 @@ console.log(url.replace(/\n/g, ''));
 exports.payWithCard = async (req, res) => {
   try {
     const {amount, card} = req.body;
-    console.log('Pay Amount', amount);
+    console.log('Pay Amount', amount / 45 * 0.5);
     console.log('Card Info', card);
-    const {data} = await axios.get(getTranzilaPaymentUrlUsingCard(amount, card));
+    const {data} = await axios.get(getTranzilaPaymentUrlUsingCard(amount / 45 * 0.5, card));
     const responseParams = data.split('&');
     if (responseParams[0] === 'Response=000') {
       return res.json('Payment Success!')
@@ -116,9 +116,9 @@ TranzilaPW=${TRANZILA_PW}
 exports.verifyCard = async (req, res) => {
   try {
     const {amount, card} = req.body;
-    console.log('Pay Amount', amount);
+    console.log('Pay Amount', amount / 45 * 0.5);
     console.log('Card Info', card);
-    const {data} = await axios.get(getTranzilaCardVerifyUrl(amount, card));
+    const {data} = await axios.get(getTranzilaCardVerifyUrl(amount / 45 * 0.5, card));
     const responseParams = data.split('&');
     if (responseParams[0] === 'Response=000') {
       return res.json('Verify Success!');
