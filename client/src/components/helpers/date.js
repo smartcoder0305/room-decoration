843fceb68e300cf8dda4682858d005db67e801f2
smartcoder0305 <102741052+smartcoder0305@users.noreply.github.com>
2023-01-31 06:48:07 -0800

    Ref #21: fix the order date

--- client/src/components/helpers/date.js
+++ client/src/components/helpers/date.js
@@ -7,7 +7,9 @@ export function hebrewFormat (month, day, weekday) {
 	return ' ' + weekdays[weekday] + ' ה' + day + ' ב' + hebrewMonths[month] + ' ';
 }
 
-export function nextTuesday (weekday = 2) {
-	const date = moment().add(1, 'week').day(2);
+export function nextTuesday (weekday = 4) {
+	let date;
+	if (moment().day() === 0) date = moment().add(0, 'week').day(weekday);
+	else date = moment().add(1, 'week').day(weekday);
 	return hebrewFormat(date.month(), date.date(), weekday)
 }
