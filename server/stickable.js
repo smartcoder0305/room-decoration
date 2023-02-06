574a040597fc097b120336ac3a9391bd7dc9cf2b
smartcoder0305 <102741052+smartcoder0305@users.noreply.github.com>
2023-02-06 01:07:22 -0800

    Fix the API

--- server/stickable.js
+++ server/stickable.js
@@ -13,13 +13,16 @@ process.on('unhandledRejection', (reason, promise) => {
 mongoose.connect(process.env.MONGO_URI);
 app.use(cors());
 app.use(bodyparser.json({limit: '5000mb'}));
-app.use('/', express.static(__dirname + '/public/front'));
+app.use(express.static('./public/front'));
 
 // app.get('/', function (req, res) {
 //     res.send('You have no permission to access');
 // })
 
  app.use("/api", apiroute);
+ app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/front/index.html'));
+});
  
  app.listen(process.env.PORT, function () {
     
